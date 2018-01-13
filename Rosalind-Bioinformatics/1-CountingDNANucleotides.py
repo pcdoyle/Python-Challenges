605891b77545189b83e3ae90ea88b19f31027fc6
Patrick Doyle <accounts@pcdoyle.com>
2018-01-12 21:35:38 -0800

    New Projects Added

---
+++ Rosalind-Bioinformatics/1-CountingDNANucleotides.py
@@ -0,0 +1,25 @@
+"""
+__author__ = "Patrick Doyle"
+__license__ = "The Unlicense"
+__email__ = "me@pcdoyle.com"
+
+Rosalind Bioinformatics:
+    Counting DNA Nucleotides
+    http://rosalind.info/problems/dna/
+
+This should be relatively easy with Python.
+"""
+# The filename with the DNA Strings
+DNAFILE = '1-DNANucleotides.txt'
+
+def countDNA():
+    # Open the file and read the DNA strings into a list.
+    dnaString = open(DNAFILE,'r').read().splitlines() # splitlines() removes the newline character from the file.
+
+    # Loop through the list of strings and print the results to the console.
+    for i in range(len(dnaString)):
+        print(dnaString[i].count('A'),dnaString[i].count('C'),dnaString[i].count('G'),dnaString[i].count('T'))
+
+if __name__ == "__main__":
+    # Start the program.
+    countDNA()
--- Rosalind-Bioinformatics/README.md
+++ Rosalind-Bioinformatics/README.md
@@ -2,4 +2,6 @@
 You can attempt these projects at http://rosalind.info/problems/locations/.
 
 # Notes:
-* No project has been uploaded for this yet.
+* 1-CountingDNANucleotides.py
+** This was a really simple task in python.
+** I slightly expanded on the program to allow to read from a list of DNA Strings rather than just one string.
